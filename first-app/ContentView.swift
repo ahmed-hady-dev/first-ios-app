1704c5bc4f62d34b9aa810bb7425babe784032da
ahmedabd-elhady <ahmed.hady.dev@gmail.com>
2025-02-04 06:26:09 +0200

    Initial Commit

---
+++ first-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  first-app
+//
+//  Created by Ahmed Abd-ElHady on 04/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ first-app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first-app
+//
+//  Created by Ahmed Abd-ElHady on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
